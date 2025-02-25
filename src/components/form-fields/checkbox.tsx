ad0555519f2596978685e545dae5a9a8f8a788d8
ahmedafify45 <aafify326@gmail.com>
2025-02-25 08:22:07 +0200

    form-field

--- src/components/form-fields/checkbox.tsx
+++ src/components/form-fields/checkbox.tsx
@@ -2,18 +2,13 @@ import { IFormField } from "@/types/app";
 import { Label } from "../ui/label";
 import { Checkbox as ShadcnCheckbox } from "../ui/checkbox";
 
-// interface Props {
-//   onClick?: () => void;
-//   checked: boolean;
-//   // label: IFormField["label"];
-//   // name: IFormField["name"];
-// }
 interface Props {
   onClick?: () => void;
   checked: boolean;
-  label: IFormField["label"];
+  label?: IFormField["label"];
   name: IFormField["name"];
 }
+
 const Checkbox = ({ label, name, checked, onClick }: Props) => {
   return (
     <div className="text-accent flex items-center gap-2">
